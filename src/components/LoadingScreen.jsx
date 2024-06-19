a8b47b6285a07485d34b0b2d16205640dd22d0f8
Janhavi Chauhan <143059176+janhavichauhan@users.noreply.github.com>
2024-06-19 23:29:54 +0530

    Add files via upload

---
+++ src/components/LoadingScreen.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const LoadingScreen = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div>
+        <img
+          src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fdribbble.com%2Fshots%2F5757826-Loading-gif&psig=AOvVaw0BeGI6zdrDb92w7onpw3ab&ust=1718898977644000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCJDhy-2D6IYDFQAAAAAdAAAAABAE"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default LoadingScreen;
---
+++ src/constants.js
@@ -0,0 +1,54 @@
+export const techStack = [
+
+  { name: "C++", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg" },
+  {name : "Python", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg"},
+
+
+  { name: "Express", link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg" },
+
+  { name: "NodeJS", link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg" },
+  { name: "Postman", link: "https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg" },
+
+
+  
+  { name: "React Native", link: "https://pagepro.co/blog/wp-content/uploads/2020/03/react-native-logo-884x1024.png" },
+  { name: "HTML", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" },
+  { name: "CSS", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg" },
+  { name: "Bootstrap", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-plain.svg" },
+  { name: "Redux", link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg" },
+
+  
+
+  { name: "Javascript", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" },
+  { name: "Tailwind CSS", link: "https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg" },
+  { name: "React", link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" },
+  { name: "MySQl", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-plain.svg" },
+  { name: "Mongo DB", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-plain.svg" },
+  // { name: "Azure", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/azure/azure-plain.svg" },
+  { name: "Git", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain.svg" },
+  { name: "Github", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" },
+  // { name: "Docker", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-plain.svg" },
+  { name: "Firebase", link: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg" },
+  {name : " Figma", link: "https://www.vectorlogo.zone/logos/figma/figma-icon.svg"},
+  {name : "Canva", link : "https://www.vectorlogo.zone/logos/canva/canva-icon.svg"},
+
+
+
+
+  { name: "VS Code", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg" },
+  // {name: "", link: "https://repository-images.githubusercontent.com/59065830/b62be480-45d2-11ea-9989-803db0f9c44d"}
+]
+
+
+export const contactLinks = [
+
+{ name: 'Github', url: 'https://img.icons8.com/doodle/40/000000/github--v1.png', link :''},
+{ name: 'LinkedIn', url: 'https://img.icons8.com/doodle/40/000000/linkedin--v2.png', link: ''}
+]
+
+
+
+
+
+
+
